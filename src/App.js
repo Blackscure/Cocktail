020cc0865cf8cd3ff4084ca98a6dfae991ca7f17
blackscure <Wekesabuyahi@gmail.com>
2021-01-29 11:05:36 +0300

    nav bar created in app file

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
 
-export default App;
+  function App() {
+    return (
+      <div className="App">
+         <nav class="navbar navbar-light bg-light">
+         <div class="container-fluid">
+         <a class="navbar-brand">Cocktail</a>
+         <form class="d-flex">
+         <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
+         <button class="btn btn-outline-success" type="submit">Search</button>
+         </form>
+         </div>
+         </nav>
+      </div>
+    );
+  }
+
+  export default App;
