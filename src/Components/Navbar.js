733b537a3050b00068e0c7fd2955c9be2d0842b0
blackscure <Wekesabuyahi@gmail.com>
2021-02-01 16:24:15 +0300

    home created

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,14 +1,20 @@
 import React from 'react';
 
 
+import { Link } from 'react-router-dom';
+
+
+
 function Navbar() {
   return (
     <div class="navbar">
       <h3>COCTAIL</h3>
-      <a href="#Home">Home</a>
-      <a href="#View">View</a>
-  </div>
+      <Link to="/home">Home</Link>
+      <Link to="/random">Random Images</Link>
+    </div>
   )
 }
 
 export default Navbar
+
+ 
